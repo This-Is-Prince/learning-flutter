f32e1e04b0c878b06817ac5567b6f6688fa533a8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-08-09 05:10:44 +0530

    feat(Again): starting learning flutter again

--- flutter_course_freecodecamp/ch01_learningdart/android/app/src/main/kotlin/com/example/ch01_learningdart/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ch01_learningdart
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_course_freecodecamp/mynotes/lib/utils/routes.dart
+++
@@ -1,9 +0,0 @@
-enum Routes {
-  login('/login/'),
-  register('/register/'),
-  verifyEmail('/verify-email/'),
-  notes('/notes/');
-
-  const Routes(this.value);
-  final String value;
-}
